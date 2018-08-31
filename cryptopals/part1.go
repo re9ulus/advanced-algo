5093a9f09f16fff201ab01eb27d58a1016a89b12
Vasiliy Zemlyanov <re9ulusv@gmail.com>
2018-08-31 22:19:22 +0300

    fix cryptolaps -> cryptopals

--- cryptopals/part1.go
+++ cryptopals/part1.go
@@ -1,9 +1,10 @@
-package cryptolaps
+package cryptopals
 
 import b64 "encoding/base64"
 import "encoding/hex"
 
 
+// Task 1
 func hexToBase64(hexStr string) string {
 	binaryStr, err := hex.DecodeString(hexStr)
 	if err != nil {
@@ -12,6 +13,7 @@ func hexToBase64(hexStr string) string {
 	return b64.StdEncoding.EncodeToString(binaryStr)
 }
 
+// Task 2
 func xorHex(first string, second string) string {
 	if len(first) != len(second) {
 		panic("Strings must have equal length")
@@ -29,4 +31,18 @@ func xorHex(first string, second string) string {
 		buffer[idx] = firstBinary[idx] ^ secondBinary[idx]
 	}
 	return hex.EncodeToString(buffer)
+}
+
+// Task 3
+func findXorChar(input string) string {
+	byteInput, _ := hex.DecodeString(input)
+	bestMatch := ""
+	for b := 0; b < 256; b++ {
+		buffer := make([]byte, len(byteInput))
+		for idx, _ := range(byteInput) {
+			buffer[idx] = byteInput[idx] ^ byte(b)
+			// TODO: Evaluate using some metrics
+		}
+	}
+	return bestMatch
 }
--- cryptopals/part1_test.go
+++ cryptopals/part1_test.go
@@ -1,4 +1,4 @@
-package cryptolaps
+package cryptopals
 
 import "testing"
 
